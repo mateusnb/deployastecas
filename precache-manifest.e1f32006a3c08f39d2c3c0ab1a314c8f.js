3b81e1e5ee0236d7d22319a4f6e1b213e9ec566f
Mateus <mateusnb@gmail.com>
2020-07-28 01:40:28 -0300

    Updates

---
+++ precache-manifest.e1f32006a3c08f39d2c3c0ab1a314c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50805c1ff532f219fa0f48168ff02f86",
+    "url": "/deployastecas/index.html"
+  },
+  {
+    "revision": "0b11b2be844f3d948bae",
+    "url": "/deployastecas/static/css/main.c8a0655b.chunk.css"
+  },
+  {
+    "revision": "9d618ff19189ac9a4132",
+    "url": "/deployastecas/static/js/2.0c0246bc.chunk.js"
+  },
+  {
+    "revision": "0b11b2be844f3d948bae",
+    "url": "/deployastecas/static/js/main.5ff2f357.chunk.js"
+  },
+  {
+    "revision": "1c857a5f631f5ea8e9e4",
+    "url": "/deployastecas/static/js/runtime-main.6fc98dbf.js"
+  }
+]);
